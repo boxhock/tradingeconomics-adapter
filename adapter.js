ec9fa3406cffc28f2c82c3ef5593e7c528288227
Jonas Hals <jonas@smartcontract.com>
2021-01-07 14:59:17 +0100

    Use updated TE Client

--- adapter.js
+++ adapter.js
@@ -1,97 +1,97 @@
 const { Requester, Validator } = require('@chainlink/external-adapter')
-const TeClient = require('tradingeconomics-stream')
+const { TEClient } = require('tradingeconomics-stream')
 const API_CLIENT_KEY = process.env.API_CLIENT_KEY
 const API_CLIENT_SECRET = process.env.API_CLIENT_SECRET
-const subscribeSymbols = process.env.SYMBOLS || ""
+const subscribeSymbols = process.env.SYMBOLS || ''
 
-let prices = {}
+const prices = {}
 
 const subscribe = asset => {
-    const client = new TeClient({
-        url: 'ws://stream.tradingeconomics.com/',
-        key: API_CLIENT_KEY,
-        secret: API_CLIENT_SECRET,
-        reconnect: true
-    })
+  const client = new TEClient({
+    url: 'ws://stream.tradingeconomics.com/',
+    key: API_CLIENT_KEY,
+    secret: API_CLIENT_SECRET,
+    reconnect: true
+  })
 
-    client.subscribe(asset)
+  client.subscribe(asset)
 
-    client.on('message', msg => {
-        console.log(`Got price for asset ${asset}:`, msg.price)
-        prices[asset] = msg.price
-    })
+  client.on('message', msg => {
+    console.log(`Got price for asset ${asset}:`, msg.price)
+    prices[asset] = msg.price
+  })
 }
 
 const startService = () => {
-    const symbols = subscribeSymbols.split(',')
-    for (let i = 0; i < symbols.length; i++) {
-        const symbol = commonSymbols[symbols[i]]
-        subscribe(symbol)
-    }
+  const symbols = subscribeSymbols.split(',')
+  for (let i = 0; i < symbols.length; i++) {
+    const symbol = commonSymbols[symbols[i]]
+    subscribe(symbol)
+  }
 }
 
 const customParams = {
-    base: ['base', 'from', 'asset']
+  base: ['base', 'from', 'asset']
 }
 
 const commonSymbols = {
-    N225: 'NKY:IND',
-    FTSE: 'UKX:IND'
+  N225: 'NKY:IND',
+  FTSE: 'UKX:IND'
 }
 
 const createRequest = (input, callback) => {
-    const validator = new Validator(callback, input, customParams)
-    const jobRunID = validator.validated.id
-    let symbol = validator.validated.data.base.toUpperCase()
-    if (symbol in commonSymbols) {
-        symbol = commonSymbols[symbol]
+  const validator = new Validator(callback, input, customParams)
+  const jobRunID = validator.validated.id
+  let symbol = validator.validated.data.base.toUpperCase()
+  if (symbol in commonSymbols) {
+    symbol = commonSymbols[symbol]
+  }
+
+  const price = Number(prices[symbol])
+  if (price > 0) {
+    const response = {
+      data: {
+        result: price
+      },
+      result: price,
+      status: 200
     }
+    return callback(response.status, Requester.success(jobRunID, response))
+  }
 
-    const price = Number(prices[symbol])
-    if (price > 0) {
-        const response = {
-            data: {
-                result: price
-            },
-            result: price,
-            status: 200
-        }
-        return callback(response.status, Requester.success(jobRunID, response))
+  // Fall back to getting the data from HTTP endpoint
+  const url = `https://api.tradingeconomics.com/markets/symbol/${symbol}`
+
+  const params = {
+    c: `${API_CLIENT_KEY}:${API_CLIENT_SECRET}`
+  }
+
+  const config = {
+    url,
+    params
+  }
+
+  const _handleResponse = response => {
+    if (!response.data || response.data.length < 1) {
+      return callback(500, Requester.errored(jobRunID, 'no result for query'))
     }
+    // Replace array by the first object in array
+    // to avoid unexpected behavior when returning arrays.
+    response.data = response.data[0]
 
-    // Fall back to getting the data from HTTP endpoint
-    const url = `https://api.tradingeconomics.com/markets/symbol/${symbol}`
+    response.data.result = Requester.validateResultNumber(response.data, ['Last'])
+    prices[symbol] = response.data.result
+    callback(response.status, Requester.success(jobRunID, response))
+  }
 
-    const params = {
-        c: `${API_CLIENT_KEY}:${API_CLIENT_SECRET}`
-    }
+  const _handleError = error => callback(500, Requester.errored(jobRunID, error))
 
-    const config = {
-        url,
-        params
-    }
-
-    const _handleResponse = response => {
-        if (!response.data || response.data.length < 1) {
-            return callback(500, Requester.errored(jobRunID, 'no result for query'))
-        }
-        // Replace array by the first object in array
-        // to avoid unexpected behavior when returning arrays.
-        response.data = response.data[0]
-
-        response.data.result = Requester.validateResultNumber(response.data, ['Last'])
-        prices[symbol] = response.data.result
-        callback(response.status, Requester.success(jobRunID, response))
-    }
-
-    const _handleError = error => callback(500, Requester.errored(jobRunID, error))
-
-    Requester.request(config)
-        .then(_handleResponse)
-        .catch(_handleError)
+  Requester.request(config)
+    .then(_handleResponse)
+    .catch(_handleError)
 }
 
 module.exports = {
-    startService: startService,
-    createRequest: createRequest
+  startService: startService,
+  createRequest: createRequest
 }
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const bootstrap = require('@chainlink/ea-bootstrap')
 const { startService, createRequest } = require('./adapter')
 
 const run = () => {
-    startService()
-    bootstrap.server.init(createRequest)()
+  startService()
+  bootstrap.server.init(createRequest)()
 }
 
 run()
